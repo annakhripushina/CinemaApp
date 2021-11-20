ec1e2508daaceb01d6aedc1233a38926143dd56e
annakhripushina <annakhripushina@gmail.com>
2021-11-20 22:20:03 +0300

    HW_2 Final commit p.1-8

--- app/src/main/java/com/example/cinema_app/DialogBack.kt
+++ app/src/main/java/com/example/cinema_app/DialogBack.kt
@@ -13,25 +13,14 @@ class DialogBack: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(requireContext())
             .setView(R.layout.dialog_back)
-            .setNegativeButton("Нет") { dialog, which -> }//???
+            .setNegativeButton("Нет") { dialog, which -> }
             .setPositiveButton("Да") { dialog, which -> (activity as MainActivity).superOnBackPressed()} //??? finish() / super.onBackPressed()
             .create()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("TAG_Create", "CREATE")
         super.onCreate(savedInstanceState)
         isCancelable = false
-    }
-
-    override fun onCancel(dialog: DialogInterface) {
-        Log.d("TAG_Cancel", "CANCEL")
-        super.onCancel(dialog)
-    }
-
-    override fun onDismiss(dialog: DialogInterface) {
-        Log.d("TAG_Dismiss", "DISMISS")
-        super.onDismiss(dialog)
     }
 
 }
